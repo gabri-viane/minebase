d824a1384ee57ad0e6e4757bbf711a2a242e9ae1
gabri-viane <gabriviane@hotmail.it>
2022-09-15 16:18:15 +0200

    Update animations.lua

--- scripts/HUD/animations/animations.lua
+++ scripts/HUD/animations/animations.lua
@@ -45,7 +45,7 @@ function minebase.HUD.animations.injectAlphaFade(container)
         container.anim = {};
     end
     container.anim.fade_dx = {
-        clock = {
+        clockS = {
             dt = 0;
             finish = function()
                 --fix finale offset
@@ -58,6 +58,26 @@ function minebase.HUD.animations.injectAlphaFade(container)
                 fx.current = fx.current + fx.step_per_second * dtime;
                 fx.altered[4] = math.floor(fx.current);
                 container:updateElement(fx.to_fade, { { name = "text", value = table.concat(fx.altered) } });
+            end
+        },
+        clockM = {
+            dt = 0;
+            finish = function()
+                --fix finale offset
+                local fx = container.anim.fade_dx;
+                for i = 1, #fx.altered do
+                    fx.altered[i][4] = fx.to;
+                    container:updateElement(fx.to_fade[i], { { name = "text", value = table.concat(fx.altered[i]) } });
+                end
+            end,
+            tick = function(self, dtime)
+                local fx = container.anim.fade_dx;
+                fx.current = fx.current + fx.step_per_second * dtime;
+                local v = math.floor(fx.current);
+                for i = 1, #fx.altered do
+                    fx.altered[i][4] = v;
+                    container:updateElement(fx.to_fade[i], { { name = "text", value = table.concat(fx.altered[i]) } });
+                end
             end
         },
         to_fade = "", --nome elemento
@@ -81,13 +101,33 @@ function minebase.HUD.animations.injectAlphaFade(container)
             fx.to_fade = hud_image_name;
             fx.base_img = img.text;
             fx.step_per_second = (to - from) / seconds;
-            fx.clock.dt = seconds;
+            fx.clockS.dt = seconds;
             fx.current = fx.from;
 
             fx.altered = { "(", fx.base_img, ")^[opacity:", fx.from };
 
-            tx[#tx + 1] = fx.clock;
+            tx[#tx + 1] = fx.clockS;
         end
+    end
+
+    function container:alphaFadeM(hud_images, from, to, seconds)
+
+        local fx = self.anim.fade_dx;
+        fx.from = from;
+        fx.to = to;
+        fx.step_per_second = (to - from) / seconds;
+        fx.clockM.dt = seconds;
+        fx.current = fx.from;
+
+        for _, v in ipairs(hud_images) do
+            local img = self:getElement(v);
+            if img and img.type == "def" and img.hud_elem_type == "image" then
+                fx.to_fade[#fx.to_fade + 1] = hud_images;
+                fx.base_img[#fx.base_img + 1] = img.text;
+                fx.altered[#fx.altered + 1] = { "(", fx.base_img, ")^[opacity:", fx.from };
+            end
+        end
+        tx[#tx + 1] = fx.clockM;
     end
 
     return container;
@@ -156,16 +196,16 @@ function minebase.HUD.animations.injectAnimation(container)
                 if e_idx > t_len then
                     local over = tx_.max - (t_len - s_idx);
                     e_idx = t_len;
-                    for i=1, over do
+                    for i = 1, over do
                         to_add = to_add .. " ";
                     end
                 end
-                tx_.altered = tx_.base_text:sub(s_idx, e_idx) ..to_add;
+                tx_.altered = tx_.base_text:sub(s_idx, e_idx) .. to_add;
                 container:updateElement(tx_.to_animate, { { name = "text", value = tx_.altered } });
                 if math.floor(tx_.current) == t_len then
                     tx_.current = 0;
-                    for i=1, tx_.max do
-                        tx_.base_text =  " "..tx_.base_text;
+                    for i = 1, tx_.max do
+                        tx_.base_text = " " .. tx_.base_text;
                     end
                     if tx_.mode == "repeat" then
                         tx_.repeat_count = tx_.repeat_count - 1;
