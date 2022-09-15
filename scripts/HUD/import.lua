7cac5e3b6074101d383d07806373419a7e4a4c68
gabri-viane <gabriviane@hotmail.it>
2022-09-15 22:05:50 +0200

    Update import.lua

--- scripts/HUD/import.lua
+++ scripts/HUD/import.lua
@@ -1,4 +1,4 @@
 dofile(minebase.scripts..'/HUD/init.lua');
 dofile(minebase.scripts..'/HUD/container.lua');
-dofile(minebase.scripts..'/scripts/HUD/HUD.lua');
-dofile(minebase.scripts..'/scripts/HUD/animations/animations.lua');
+dofile(minebase.scripts..'/HUD/HUD.lua');
+dofile(minebase.scripts..'/HUD/animations/animations.lua');
